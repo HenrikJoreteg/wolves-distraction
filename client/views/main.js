c4adc66ecc1ec442a072f31efc9e0d76b3a1b8b2
Henrik Joreteg <hjoreteg@gmail.com>
2014-10-01 11:48:43 -0700

    11:48

--- client/views/main.js
+++ client/views/main.js
@@ -1,7 +1,19 @@
 var View = require('ampersand-view');
+var PageSwitcher = require('ampersand-view-switcher');
+var templates = require('../templates');
 
 
 module.exports = View.extend({
-    template: '<body><h1>Terrible Template</h1></body>',
-    autoRender: true
+    template: templates.body,
+    autoRender: true,
+    initialize: function () {
+        this.listenTo(app.router, 'page', this.handleNewPage);
+    },
+    render: function () {
+        this.renderWithTemplate(this);
+        this.pages = new PageSwitcher(this.queryByHook('page-container'));
+    },
+    handleNewPage: function (page) {
+        this.pages.set(page);
+    }
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer(3000, 'localhost');
 var moonboots = require('moonboots_hapi');
+var templatizer = require('templatizer');
 
 
 server.pack.register({
@@ -9,7 +10,13 @@ server.pack.register({
         appPath: '/{p*}',
         moonboots: {
             main: __dirname + '/client/app.js',
-            developmentMode: true
+            developmentMode: true,
+            stylesheets: [
+                __dirname + '/styles/bootstrap.css'
+            ],
+            beforeBuildJS: function () {
+                templatizer(__dirname + '/templates', __dirname + '/client/templates.js');
+            }
         }
     }
 }, function () {
