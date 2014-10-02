12bd38a27e679da0bc82dcc560a7a05f96fe5225
Henrik Joreteg <hjoreteg@gmail.com>
2014-10-02 15:01:11 -0700

    3:01

--- client/models/howls.js
+++ client/models/howls.js
@@ -1,5 +1,6 @@
 var Collection = require('ampersand-rest-collection');
 var Howl = require('./howl');
+var playAudio = require('howlinfool');
 
 
 module.exports = Collection.extend({
@@ -7,8 +8,23 @@ module.exports = Collection.extend({
     model: Howl,
     initialize: function () {
         this.fetch();
+        this.fetchRealtime();
     },
     comparator: function (model) {
         return -1 * model.createdAt.valueOf();
+    },
+    fetchRealtime: function () {
+        var self = this;
+        var socket = new WebSocket('ws://wolves.technology');
+
+        socket.onmessage = function (event) {
+            var data = JSON.parse(event.data);
+
+            if (data.channel === self.url && data.action === 'update') {
+                playAudio();
+                console.log('Got a howl with id', data.id);
+                self.fetchById(data.id);
+            }
+        };
     }
 });
